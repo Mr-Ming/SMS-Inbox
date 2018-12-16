34b7c54181a58ce6c5c3a24526b30281033636d3
Mr_ming <youminghou@gmail.com>
2018-12-16 13:00:05 -0500

    Initial Commit

---
+++ src/components/SmsInbox.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class SmsInbox extends Component {
+	render() {
+		return (
+			<div className="SmsInbox">
+				Box
+			</div>
+		);
+	}
+}
+
+export default SmsInbox;
